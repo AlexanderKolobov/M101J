0a6137943a2ff7ca714cb681237a467c634b7049
Alexander Kolobov <alexander.kolobov@hys-enterprise.com>
2018-04-24 17:59:20 +0300

    week 3 finish home work

--- src/main/java/com/mongodb/week_3/hw3/HW_3_1.java
+++ src/main/java/com/mongodb/week_3/hw3/HW_3_1.java
@@ -6,16 +6,28 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static com.mongodb.client.model.Aggregates.match;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.util.Helpers.printJson;
+
 /**
  * Created by Alexander Kolobov on 4/10/2018.
  */
+//TODO finish this!!!
 public class HW_3_1 {
     public static void main(String[] args) {
         MongoClient client = new MongoClient(new ServerAddress("localhost", 27017));
 
-        MongoDatabase database = client.getDatabase("students");
-        final MongoCollection<Document> collection = database.getCollection("grades");
+        MongoDatabase database = client.getDatabase("school");
+        final MongoCollection<Document> students = database.getCollection("students");
 
-
+        ArrayList<Document> homework = students.aggregate(Arrays.asList(match(eq("scores.type", "homework")))).into(new ArrayList<>());
+        System.out.println(homework.size());
+        for (int i = 0; i < 2; i++) {
+            printJson(homework.get(i));
+        }
     }
 }
